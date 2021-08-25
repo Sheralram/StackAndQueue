6ea58a2b8781e253e2d7a900ab6b9d03bbea7506
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-26 00:31:55 +0530

    Welcome to STACK & QUEUE program

---
+++ src/com/bridgelabz/StackAndQueue.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class StackAndQueue {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome Stack and Queue program");
+    }
+
+
+}
