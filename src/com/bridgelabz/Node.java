0744db6adbaa3d75f86bf3d270c0d35b0ac20846
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-26 23:16:21 +0530

    PEAKandPOP : till Stack is Empty

--- src/com/bridgelabz/Node.java
+++ src/com/bridgelabz/Node.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
- class Node {
+class Node {
 
     int data;
     Node next;
@@ -8,5 +8,58 @@ package com.bridgelabz;
     Node(int data) {
         this.data = data;
         this.next = null;
+    }
+}
+
+class StackAndQueue {                   // Program for Stack
+    Node top;
+
+
+    public void push(int x) {
+        Node newNode = new Node(x);
+        if (isEmpty()) {
+            top = newNode;
+        } else {
+            newNode.data = x;
+            newNode.next = top;
+            top = newNode;
+        }
+    }
+
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void display() {
+        if (top == null) {
+            System.out.printf("\nStack is Empty and Underflow");
+        } else {
+            Node temp = top;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void pop() {
+        if (isEmpty()) {
+            System.out.print("\n Stack is EMPTY and Underflow");
+            return;
+        }
+        for (int i = 0; i < 3; i++) {
+            top = top.next;
+        }
+    }
+
+    public int peek() {
+        if (!isEmpty()) {
+            System.out.println("Top data is :" + top.data);
+            return top.data;
+        } else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
     }
 }
