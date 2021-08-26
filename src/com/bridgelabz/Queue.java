3995b5c837e1696fc35f110883261a7819fe9457
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-26 23:20:08 +0530

    ENQUEUE:added 3 elements in QUEUE

--- src/com/bridgelabz/Queue.java
+++ src/com/bridgelabz/Queue.java
@@ -1,5 +1,44 @@
 package com.bridgelabz;
 
 public class Queue {
+    Node front,rear;
 
+    public void enqueue(int x) {                                         // Enqueue in Queue
+        Node newNode=new Node(x);
+        if(isEmpty()){
+            rear = newNode;
+            front = newNode;
+        }
+        else{
+            rear.next = newNode;
+            rear = newNode;
+        }
+    }
+
+    public boolean isEmpty() {
+
+        return rear == null;
+    }
+
+    public void display() {
+        if (isEmpty()) {
+            System.out.print("\n Queue is empty. Add data in Queue");
+        }
+        else {
+            Node temp = front;
+            while (temp != null) {
+                System.out.print(temp.data+ " ");
+                temp = temp.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue(56);
+        queue.enqueue(30);
+        queue.enqueue(70);
+        queue.display();
+
+    }
 }
